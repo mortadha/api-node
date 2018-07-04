ede5e2440aba0cadbb56079604d418f6219fc359
amanisaid <amanisaid740@gmail.com>
2018-07-04 12:52:02 +0100

    params

--- routes/users.js
+++ routes/users.js
@@ -162,8 +162,8 @@ router.get('/getMyEvents', function(req, res, next) {
 });
 
 /* get events. */
-router.get('/getMycours', function(req, res, next) {
-  id = req.body.id;
+router.get('/getMycours/:id', function(req, res, next) {
+  id = req.params.id;
   event.getMyCours(id, function(err,events){
     if(err){
       throw err;
