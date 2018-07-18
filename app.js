4306a4d4ee7613e3175cd1b6aa64377fce665b73
mortadha <morta.est@hotmail.fr>
2018-07-18 13:54:25 +0100

    app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var annonceRouter = require('./routes/annonce');
 var bodyParser = require('body-parser')
 var app = express();
 
@@ -16,6 +15,10 @@ app.set('view engine', 'jade');
 mongoose.connect('mongodb://rania:PNETJ9t13FZyO5B55yIQOikeV9cXgslKLlBC06YrZC0GSh61hhbx4RAf8OJg62iuMRiaVQ4DHYC0fltAvF2Z0Q%3D%3D@rania.documents.azure.com:10255/?ssl=true');
 var db = mongoose.connections;
 
+/*mongoose.connect('mongodb://localhost/fflokv14');
+var db = mongoose.connections;*/
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,20 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-var io = require('socket.io').listen(app.listen(3333));
-io.sockets.on('connection', function (socket) {
-  console.log('client connect');
-  socket.on('echo', function (data) {
-  io.sockets.emit('message', data);
-});
-});
-app.post('/message', function(req, res, next) {
-  var message = req.body.message;
-  io.sockets.emit('message', message);
-  return res.status(200).json({'message': true});
-});
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+app.use('/api', annonceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,5 +46,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-console.log('rania 2');
+
 module.exports = app;
