e31f5cfbec974cb1c95b6f8b41cca61f8375ea40
amanisaid <amanisaid740@gmail.com>
2018-07-18 14:05:04 +0100

    first push

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var annonceRouter = require('./routes/annonce');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var bodyParser = require('body-parser')
 var app = express();
 
@@ -15,10 +16,6 @@ app.set('view engine', 'jade');
 mongoose.connect('mongodb://rania:PNETJ9t13FZyO5B55yIQOikeV9cXgslKLlBC06YrZC0GSh61hhbx4RAf8OJg62iuMRiaVQ4DHYC0fltAvF2Z0Q%3D%3D@rania.documents.azure.com:10255/?ssl=true');
 var db = mongoose.connections;
 
-/*mongoose.connect('mongodb://localhost/fflokv14');
-var db = mongoose.connections;*/
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,9 +24,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+var io = require('socket.io').listen(app.listen(3333));
+io.sockets.on('connection', function (socket) {
+  console.log('client connect');
+  socket.on('echo', function (data) {
+  io.sockets.emit('message', data);
+});
+});
 
-
-app.use('/api', annonceRouter);
+app.use('/', indexRouter);
+app.use('/api', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,5 +50,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+console.log('rania');
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -6,239 +6,37 @@ event = require('../models/event.js');
 categorie = require('../models/categorie.js');
 
 var bcrypt = require('bcrypt');
+var fs = require('fs');
+var parse = require('xml-parser');
+
+var multer  = require('multer');
+
+//config multer for upload video
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '--' + file.originalname)
+    }
+  });
+
+ 
+
+  var upload = multer({ storage: storage })
+
 
 /* add admin. */
-router.post('/addAdmin', function(req, res, next) {
- 
-  let password = req.body.password;
-  bcrypt.hash(password,5, function(err, bcryptedPassword) {
-    let admin = {};
-    admin.role = "admin";
-    admin.name = req.body.name;
-    admin.prenon = req.body.prenon;
-    admin.phone = req.body.phone;
-    admin.adresse = req.body.adresse;
-    admin.email = req.body.email;
-    admin.pseudo = req.body.pseudo;
-    admin.password = bcryptedPassword;
-    users.addUser(admin,function(err,callback){
-      console.log(err)
-      return res.status(200).json({'user': callback});
-    });
-  });
-  
+router.post('/xml',upload.single('xml'), function(req, res, next) {
+  var video_name = req.file.fieldname + '--' + req.file.originalname;
+  console.log(video_name);
+  var xml = fs.readFileSync('C:/Users/user/Downloads/0024-01.xml', 'utf8');
+console.log("ici")
+  var inspect = require('util').inspect;
+var obj = parse(xml);
+console.log(inspect(obj, { colors: true, depth: Infinity }));
 });
 
-/* add tuteur. */
-router.post('/addTuteur', function(req, res, next) {
-  console.log("ici");
-  console.log(req.body);
-  let password = req.body.password;
-  bcrypt.hash(password,5, function(err, bcryptedPassword) {
-    let admin = {};
-    admin.role = "tuteur";
-    admin.name = req.body.name;
-    admin.prenon = req.body.prenon;
-    admin.phone = req.body.phone;
-    admin.adresse = req.body.adresse;
-    admin.email = req.body.email;
-    admin.pseudo = req.body.pseudo;
-    admin.password = bcryptedPassword;
-      
-    users.addUser(admin,function(err,callback){
-      return res.status(200).json({'user': callback});
-    });
-  });
-});
-
-  /* add apprenant. */
-router.post('/addApprenant', function(req, res, next) {
- 
-  let password = req.body.password;
-  bcrypt.hash(password,5, function(err, bcryptedPassword) {
-    let admin = {};
-    admin.role = "apprenant";
-    admin.name = req.body.name;
-    admin.prenon = req.body.prenon;
-    admin.phone = req.body.phone;
-    admin.adresse = req.body.adresse;
-    admin.email = req.body.email;
-    admin.pseudo = req.body.pseudo;
-    admin.password = bcryptedPassword;
-      
-    users.addUser(admin,function(err,callback){
-      return res.status(200).json({'user': callback});
-    });
-  });
-  
-});
-
- /* add apprenant. */
- router.post('/login', function(req, res, next) {
- 
-  let password = req.body.password;
-  
-   let pseudo = req.body.pseudo;
-   users.login(pseudo,function(err,callback){
-     if(callback.length > 0){
-      bcrypt.compare(password, callback[0].password, function(err, resbcrypt) {
-         if(resbcrypt){
-            res.status(200).json({
-              'success':true,
-              'result':{
-              'admin' :callback[0],
-              }
-              });
-          }
-          else{
-            return res.status (400).json ({'error': 'login inccorect'});
-          }
-      });
-      }
-      else{
-        return res.status(404).json({ 'error': 'Admin not exist in DB' });
-      }
-        
-    });
- 
-  
-});
-
- /* add cour. */
- router.post('/addCours', function(req, res, next) {
-  let cour = {}
-  cour.name = req.body.name;
-  cour.description = req.body.description;
-  cour.objectifs = req.body.objectifs;
-  cour.mots_clé = req.body.mots_clé;
-  cour.plan = req.body.plan;
-  cour.uploaded_by = req.body.uploaded_by;
-  
-  
-  cours.addCours(cour,function(err,callback){
-    return res.status(200).json({'user': callback});
-  });
-});
-
-
- /* add event. */
- router.post('/addEvent', function(req, res, next) {
-  let evenement = {}
-  evenement.name = req.body.name;
-  evenement.description = req.body.description;
-  evenement.h_debut = req.body.h_debut;
-  evenement.h_fin = req.body.h_fin;
-  evenement.uploaded_by = req.body.uploaded_by;
- 
-  evenement.cours = req.body.cours;
-  event.addEvent(evenement,function(err,callback){
-    return res.status(200).json({'event': callback});
-  });
-});
-
-/* get events. */
-router.get('/getEvents', function(req, res, next) {
-  event.getEvent( function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
-
-/* get events. */
-router.get('/getMyEvents/:id', function(req, res, next) {
-  var id = req.params.id;
-  event.getMyEvents(id, function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
-/* get events. */
-router.delete('/deleteEvents/:id', function(req, res, next) {
-  var id = req.params.id;
-  event.deleteEvents(id, function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
-
-/*myProfil. */
-router.get('/myProfil/:id', function(req, res, next) {
-  var id = req.params.id;
-  console.log(id)
-  users.myProfil(id, function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
-/*myProfil. */
-router.post('/updateMyProfil/', function(req, res, next) {
-  var id = req.body.id;
-  var profil = req.body;
-  users.updateMyProfil(id,profil, function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
-
-
-/* get events. */
-router.get('/getEvent/:id', function(req, res, next) {
-  id = req.params.id;
-
-  event.getEvent(id, function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
-/* get events. */
-router.get('/getMycours/:id', function(req, res, next) {
-  id = req.params.id;
-  cours.getMyCours(id, function(err,events){
-    if(err){
-      throw err;
-    }
-    return res.status(200).json(events);
-  });
-});
-
- /* add addCategorie. */
- router.post('/addCategorie', function(req, res, next) {
-  let cat = {}
-  cat.name = req.body.name;
-  cat.description = req.body.description;
- 
-
-  categorie.addCategorie(cat,function(err,callback){
-    return res.status(200).json({'event': callback});
-  });
-});
-
-
-/* findCours. */
-router.post('/findCours', function(req, res, next) {
- search = req.body.text;
- cours.findCours(search,function(err,callback){
-    return res.status(200).json({'event': callback});
-  });
-});
 
 
 module.exports = router;
