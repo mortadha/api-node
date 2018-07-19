a95d15a5a22389dac7c6c257ac0b320c46f88eb8
mortadha <morta.est@hotmail.fr>
2018-07-19 02:47:59 +0200

    Update annonce.js

--- routes/annonce.js
+++ routes/annonce.js
@@ -12,7 +12,7 @@ var multer  = require('multer');
 //config multer for upload video
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/')
+      cb(null, '../public//')
     },
     filename: function (req, file, cb) {
       cb(null,  file.originalname)
