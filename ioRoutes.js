296b9b5349c68b05bb637a22011e389649b396b8
mortadha <morta.est@hotmail.fr>
2018-08-24 00:06:17 +0200

    Update ioRoutes.js

--- ioRoutes.js
+++ ioRoutes.js
@@ -227,5 +227,5 @@ module.exports = function(io) {
     async function asyncForEach(array, callback) {
       for (let index = 0; index < array.length; index++) {
         await callback(array[index], index, array)
-      }
-    }*/
+      }*/
+    }
