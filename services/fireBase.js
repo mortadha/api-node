7e04e112621784b9e62c504263e530309e00ae73
mortadha <morta.est@hotmail.fr>
2019-04-23 22:08:36 +0200

    delete firbase

--- services/fireBase.js
+++ services/fireBase.js
@@ -1,25 +1,18 @@
-var admin = require("firebase-admin");
+//var admin = require("firebase-admin");
 
-var serviceAccount = require("../firebase/fflok-1529511801837-firebase-adminsdk-ve1bg-bcef41c5e6.json");
+//var serviceAccount = require("../firebase/fflok-1529511801837-firebase-adminsdk-ve1bg-bcef41c5e6.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fflok-1529511801837.firebaseio.com"
-});
+//admin.initializeApp({
+ // credential: admin.credential.cert(serviceAccount),
+ // databaseURL: "https://fflok-1529511801837.firebaseio.com"
+//});
 
 
 
 
 module.exports =  {
   sendNotifaction : function sendNotifaction (Message,registrationToken) {
-    admin.messaging().sendToDevice(registrationToken,Message)
-    .then((response) => {
-      // Response is a message ID string.
-      console.log('Successfully sent message:', response);
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error);
-    });
+   
     return 'test';
   }
 }
